ebeec7f4f1dcecab912d143b832e0bd3743c0185
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2022-10-31 20:34:48 -0300

    Responsiveness in section main__client

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,21 @@
-"use strict"
-const gulp = require('gulp');
+//lista de dependencias
+const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const prefixer = require('gulp-autoprefixer');
+const mimify = require('gulp-cssnano');
 
-sass.compiler = require('node-sass');
-
-gulp.task('default', watchTask);
-gulp.task('sass', scssTask);
-
-//Compile scss into css
-function scssTask() {
-    // 1. Where is my scss file
-    return gulp
-        .src('./assets/scss/**/*.scss')
-        // 2. Pass that file through sass compiler
-        .pipe(sass({
-            outputStyle: 'compressed'
-        }).on('error', sass.logError))
-        // 3. Where do I save the compiled CSS?
-        .pipe(gulp.dest('./dist/css'));
+//criar funções
+//scss
+function compileScss(){
+    return src('./src/scss/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(prefixer())
+    .pipe(mimify())
+    .pipe(dest('./dist'))
 }
-
+//criar watch task
 function watchTask(){
-    gulp.watch('./assets/scss/**/*.scss', scssTask);
+    watch('src/scss/style.scss',compileScss);
 }
+//default gulp
+exports.default = series(watchTask);
