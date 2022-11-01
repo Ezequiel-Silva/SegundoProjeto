a07276ae5c38636619d179c9b1ca68ab0bfd65fd
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2022-11-01 16:12:36 -0300

    Adjusting card size

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,33 @@
-//lista de dependencias
-const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const cleanCss = require("gulp-clean");
-const prefix = require('gulp-autoprefixer');
-const browserSync = require("browser-sync");
-const server = browserSync.create();
+const gulp = require('gulp')
+const sass = require('gulp-sass')(require('sass'))
+const browserSync = require('browser-sync').create()
+
+// Compile sass into css
+const style = () => {
+
+    return gulp.src('./assets/scss/**/*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./styles'))
+        .pipe(browserSync.stream())
+}
+
+const watch = () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+
+    gulp.watch('./assets/scss/**/*.sass', style)
+    gulp.watch('./*.html').on('change', browserSync.reload)
+    gulp.watch('./assets/js/**/*.js').on('change', browserSync.reload)
+}
+
+exports.style = style
+exports.watch = watch
 
 /* SASS interpreta os os arquivos começados 
  * com underline como parciais. 
  * Parte das folhas de estilo escritas 
  * em SASS é formada de vários arquivos 
- * parciais e uma versão "compilada" deles.
-*/
-
-//criar funções
-//scss
-const sassTask = () =>
-    src("assets/scss/*.scss")
-    .pipe(sass().on("error", sass.logError))
-    .pipe(prefix())
-    .pipe(cleanCss({ compatibility: "ie8" }))
-    .pipe(dest("css/templates"));
-//criar watch task
-const watchTask = () =>
-    watch(['assets/scss/*.scss'],series(sassTask,reload));
-//criar task reload
-const serve = (done) => {
-    server.init({
-        proxy: "http://127.0.0.1:5500/",
-    });
-    done();
-};
-
-const reload = (done) => {
-    server.reload();
-    done();
-};
-//default gulp
-exports.default = series(serve, watchTask);
+ * parciais e uma versão "compilada" deles.*/
