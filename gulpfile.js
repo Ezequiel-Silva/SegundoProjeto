c359042da32c6756532d5e5dcb01c8f2c9f429b9
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2022-11-01 10:33:52 -0300

    Place the background in the sections of the site

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,40 @@
 //lista de dependencias
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const prefixer = require('gulp-autoprefixer');
-const mimify = require('gulp-cssnano');
+const cleanCss = require("gulp-clean");
+const prefix = require('gulp-autoprefixer');
+const browserSync = require("browser-sync");
+const server = browserSync.create();
+
+/* SASS interpreta os os arquivos começados 
+ * com underline como parciais. 
+ * Parte das folhas de estilo escritas 
+ * em SASS é formada de vários arquivos 
+ * parciais e uma versão "compilada" deles.
+*/
 
 //criar funções
 //scss
-function compileScss(){
-    return src('./src/scss/style.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(prefixer())
-    .pipe(mimify())
-    .pipe(dest('./dist'))
-}
+const sassTask = () =>
+    src("assets/scss/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(prefix())
+    .pipe(cleanCss({ compatibility: "ie8" }))
+    .pipe(dest("css/templates"));
 //criar watch task
-function watchTask(){
-    watch('src/scss/style.scss',compileScss);
-}
+const watchTask = () =>
+    watch(['assets/scss/*.scss'],series(sassTask,reload));
+//criar task reload
+const serve = (done) => {
+    server.init({
+        proxy: "http://127.0.0.1:5500/",
+    });
+    done();
+};
+
+const reload = (done) => {
+    server.reload();
+    done();
+};
 //default gulp
-exports.default = series(watchTask);
+exports.default = series(serve, watchTask);
