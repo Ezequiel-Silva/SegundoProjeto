ffb9c16192e557e2efa924832fb725d1508afe38
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2022-11-21 17:01:05 -0300

    Putting action on navItem and arrow on feedback section

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("main__feedback__item");
+    let dots = document.getElementsByClassName("main__feedback__navItem");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" main__feedback--white", "");
+    }
+    x[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " main__feedback--white";
+}
