9ca31353df795dc3024ff83f5279b4b1c1147cc0
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2022-11-30 09:56:38 -0300

    Adding mask on p´hone for modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Slider
 let slideIndex = 1;
 
 showDivs(slideIndex);
@@ -51,4 +52,18 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+// Mask tel
+const handlePhone = (event) => {
+    let input = event.target
+    input.value = phoneMask(input.value)
+}
+
+const phoneMask = (value) => {
+    if (!value) return ""
+    value = value.replace(/\D/g,'')
+    value = value.replace(/(\d{2})(\d)/,"($1) $2")
+    value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+    return value
 }
