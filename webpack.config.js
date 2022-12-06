cf955ed987f2048cdaf6852f9acd4d1fe7f35288
Ezequiel-Silva <ezequiel.ne@fatec.sp.gov.br>
2022-12-06 16:22:00 -0300

    Configuring html file in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PATH = {
+    dist: path.resolve(__dirname, "./dist")
+};
 
 module.exports = {
     mode: 'production',
+    // mode: 'development',
     entry: './src/script.js',
     output: {
         filename: 'assets/js/[name].bundle.js',
-        path: path.resolve(__dirname, './dist')
+        path: PATH.dist
     },
     module: {
         rules: [
@@ -14,20 +19,25 @@ module.exports = {
                 test: /\.(png|jpg|jpeg|gif|webp|svg)$/i,
                 type: "asset/resource",
                 generator:{
-                    filename: 'assets/images/[name][ext]'
+                    filename: 'image/[name][ext]'
                 }
             },
             { 
                 test: /\.s[ac]ss$/i, 
                 use:[ 
-                    // Creates 'style' nodes from JS strings
+                    // Cria nós de estilo a partir de strings JS
                     "style-loader",
-                    // Translates CSS into CommonJS
+                    // Traduz CSS para CommonJS
                     "css-loader",
-                    // Compiles Sass to CSS
+                    // Compila Sass para CSS
                     "sass-loader",
                 ],
             },
         ],
     },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: 'src/index.html'
+        })
+    ]
 };
